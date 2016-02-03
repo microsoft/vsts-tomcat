9ad219b96450d038554909fb0248041f3411dae0
Krishna Aditya <krishadi@microsoft.com>
2016-02-04 01:00:48 +0530

    gulp and npm files needed for building the code. corresponding gitignore

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var del = require("del");
+var gulp = require("gulp");
+var tsb = require("gulp-tsb");
+
+var buildDirectory = "_build";
+var sourcePaths = {
+    typescriptFiles: "src/**/*.ts",
+    copyFiles: ["src/Tasks/**/*.json", "src/Tasks/**/*.md"],
+    tasksPath: "src/Tasks"
+};
+
+var compilation = tsb.create({
+    target: 'es5',
+    module: 'commonjs',
+    declaration: false,
+    verbose: false
+});
+
+gulp.task("clean", function() {
+    return del([buildDirectory]);
+});
+
+gulp.task("compile", function () {
+    return gulp.src([sourcePaths.typescriptFiles], { base: "." })
+        .pipe(compilation())
+        .pipe(gulp.dest(buildDirectory));
+});
+
+gulp.task("build", ["compile"], function() {
+    return gulp.src(sourcePaths.copyFiles, { base: "." })
+        .pipe(gulp.dest(buildDirectory));
+});
+
+gulp.task("default", ["build"]);
