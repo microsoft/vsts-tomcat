102185f575340d71244e266df2897fdfe1251bd2
Krishna Aditya <krishadi@microsoft.com>
2016-02-09 15:20:36 +0530

    getting input from task-lib

--- src/tasks/tomcatDeployment/tomcatDeployment.ts
+++ src/tasks/tomcatDeployment/tomcatDeployment.ts
@@ -1,6 +1,24 @@
+/// <reference path="../../../typings/vsts-task-lib/vsts-task-lib.d.ts" />
+
+import tl = require("vsts-task-lib/task");
+
 export function deploy(): void {
-    this.deployWarFile();
+    var tomcatUrl = tl.getInput("tomcatUrl", true);
+    var username = tl.getInput("username", true);
+    var password = tl.getInput("password", true);
+    var warfile = tl.getInput("warfile", true);
+    var context = tl.getInput("context", true);
+    var serverVersion = tl.getInput("serverVersion", true);
+        
+    tl.debug("tomcatUrl: " + tomcatUrl);
+    tl.debug("username: " + username);
+    tl.debug("password: " + password);
+    tl.debug("warfile: " + warfile);
+    tl.debug("context: " + context);
+    tl.debug("serverVersion: " + serverVersion);
+    
+    this.deployWarFile(tomcatUrl, username, password, warfile, context, serverVersion);
 }
 
-export function deployWarFile(): void {
+export function deployWarFile(tomcatUrl: string, username: string, password: string, warfile: string, context: string, serverVersion: string): void {
 }
--- tests/tasks/tomcatDeployment/tomcatDeploymentTests.ts
+++ tests/tasks/tomcatDeployment/tomcatDeploymentTests.ts
@@ -1,21 +1,49 @@
 /// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="../../../typings/vsts-task-lib/vsts-task-lib.d.ts" />
 
 import * as tomcat from "../../../src/tasks/tomcatDeployment/tomcatDeployment";
 
 import chai = require("chai");
 import sinon = require("sinon");
 import sinonChai = require("sinon-chai");
+import tl = require("vsts-task-lib/task");
 
 chai.should();
 chai.use(sinonChai);
 
 describe("tomcat.deploy", (): void => {
-    it("should call deployWarFile", (): void => {
+    
+    function suppressTaskLibOutput(): void {
+        var stdoutmock = {
+            write: function(message: string) {}
+        };
+        tl.setStdStream(stdoutmock);
+        tl.setErrStream(stdoutmock);
+    }
+    
+    it("should pass input to deployWarFile", (): void => {
         var sandbox = sinon.sandbox.create();
-        var deployWarFileSpy = sinon.spy(tomcat, "deployWarFile");
+        var deployWarFileSpy = sandbox.spy(tomcat, "deployWarFile");
+        var getInputStub = sandbox.stub(tl, "getInput");
         
+        suppressTaskLibOutput();
+        
+        var tomcatUrl = "http://localhost:8080";
+        var username = "dummyusername";
+        var password = "dummy password";
+        var warfile = "\\users\\dummyusername\\dummywarfile.war";
+        var context = "/dummycontext";
+        var serverVersion = "6.x";
+        
+        getInputStub.withArgs("tomcatUrl").returns(tomcatUrl);
+        getInputStub.withArgs("username").returns(username);
+        getInputStub.withArgs("password").returns(password);
+        getInputStub.withArgs("warfile").returns(warfile);
+        getInputStub.withArgs("context").returns(context);
+        getInputStub.withArgs("serverVersion").returns(serverVersion);
+            
         tomcat.deploy();
         
-        deployWarFileSpy.should.have.been.calledOnce;
+        deployWarFileSpy.withArgs(tomcatUrl, username, password, warfile, context, serverVersion).should.have.been.calledOnce;
     });
 });
